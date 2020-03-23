634f8eeafd0fe6e257891fdaf1211467fb38dd9b
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-03-23 08:27:29 -0700

    Submit Quote API with basic form validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import RatingInformation from './components/RatingInformation';
 
 class App extends Component{
+  state = {
+    errors: {}
+  }
 
-  handleSubmit = (values) => {
-    console.log(values)
+  handleSubmit = async (values) => {
+    try{
+      this.setState({errors: {}}) //reset error
+      let { data } = await axios.post('https://fed-challenge-api.sure.now.sh/api/v1/quotes', values)
+      console.log(data)
+    }
+    catch(error){
+      if(error.request){
+        this.setState({errors: JSON.parse(error.request.response).errors}) //update error from API
+      }
+    }
   }
 
   render(){
     return (
       <div className="App">
-        <RatingInformation parentHandleSubmit={this.handleSubmit}/>
+        <RatingInformation parentErrors={this.state.errors} parentHandleSubmit={this.handleSubmit}/>
       </div>
     );
   }
--- src/components/RatingInformation.js
+++ src/components/RatingInformation.js
@@ -4,11 +4,27 @@ import { AccountCircle } from '@material-ui/icons';
 
 export default class RatingInformation extends Component {
     // State declarations
-    state = {}
+    state = {
+        first_name: "John",
+        last_name: "Doe",
+        address: {
+            line_1: "123 Drive",
+            line_2: "3B",
+            city: "San Francisco",
+            region: "CA",
+            postal: 94422,
+        }
+    }
 
     // Update state
     handleOnChange = (label, value) => {
+        console.log(this.props.parentErrors)
         this.setState({[label]: value})
+    }
+
+    // Remove extra spaces (beginning & end)
+    handleOnBlur = (label, value) => {
+        this.setState({[label]: value.trim()})
     }
 
     // Sends state values to parent
@@ -20,26 +36,43 @@ export default class RatingInformation extends Component {
     render(){
         return (
             <form onSubmit={this.handleOnSubmit}>
-                {/* FIRST NAME */}
-                <Grid container spacing={1} alignItems="flex-end">
-                    <Grid item>
-                        <AccountCircle />
+                <div>
+                    <h5>Rating Information</h5>
+                    {/* FIRST NAME */}
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <AccountCircle />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                required
+                                id="input-with-icon-grid" 
+                                label="First Name" 
+                                onChange={(e) => this.handleOnChange("first_name", e.target.value)}
+                                onBlur={(e) => this.handleOnBlur("first_name", e.target.value.trim())}
+                                value={this.state.first_name}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <TextField id="input-with-icon-grid" label="First Name" onChange={(e) => this.handleOnChange("first_name", e.target.value)} value={this.state.first_name}/>
-                    </Grid>
-                </Grid>
 
-                {/* LAST NAME */}
-                <Grid container spacing={1} alignItems="flex-end">
-                    <Grid item>
-                        <AccountCircle />
+                    {/* LAST NAME */}
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <AccountCircle />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                required
+                                id="input-with-icon-grid" 
+                                label="Last Name"
+                                onChange={(e) => this.handleOnChange( "last_name", e.target.value)}
+                                onBlur={(e) => this.handleOnBlur("last_name", e.target.value.trim())} 
+                                value={this.state.last_name}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <TextField id="input-with-icon-grid" label="Last Name" onChange={(e) => this.handleOnChange( "last_name", e.target.value)} value={this.state.last_name}/>
-                    </Grid>
-                </Grid>
-
+                </div>
+                
                 <div>
                     <h5>Address</h5>
                     {/* LINE_1 */}
@@ -48,7 +81,13 @@ export default class RatingInformation extends Component {
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="Line 1" onChange={(e) => this.handleOnChange("address", {...this.state.address, "line_1": e.target.value})} value={this.state.address && this.state.address.line_1}/>
+                            <TextField 
+                                required
+                                id="input-with-icon-grid"
+                                label="Line 1" 
+                                onChange={(e) => this.handleOnChange("address", {...this.state.address, "line_1": e.target.value})}
+                                onBlur={(e) => this.handleOnChange("address", {...this.state.address, "line_1": e.target.value.trim()})} 
+                                value={this.state.address && this.state.address.line_1}/>
                         </Grid>
                     </Grid>
 
@@ -58,7 +97,13 @@ export default class RatingInformation extends Component {
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="Line 2" onChange={(e) => this.handleOnChange("address", {...this.state.address, "line_2": e.target.value})} value={this.state.address && this.state.address.line_2}/>
+                            <TextField
+                                id="input-with-icon-grid" 
+                                label="Line 2" 
+                                onChange={(e) => this.handleOnChange("address", {...this.state.address, "line_2": e.target.value})}
+                                onBlur={(e) => this.handleOnChange("address", {...this.state.address, "line_2": e.target.value.trim()})}
+                                value={this.state.address && this.state.address.line_2}
+                            />
                         </Grid>
                     </Grid>
 
@@ -68,7 +113,14 @@ export default class RatingInformation extends Component {
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="City" onChange={(e) => this.handleOnChange("address", {...this.state.address, "city": e.target.value})} value={this.state.address && this.state.address.city}/>
+                            <TextField
+                                required
+                                id="input-with-icon-grid" 
+                                label="City" 
+                                onChange={(e) => this.handleOnChange("address", {...this.state.address, "city": e.target.value})}
+                                onBlur={(e) => this.handleOnChange("address", {...this.state.address, "city": e.target.value.trim()})} 
+                                value={this.state.address && this.state.address.city}
+                            />
                         </Grid>
                     </Grid>
 
@@ -78,7 +130,14 @@ export default class RatingInformation extends Component {
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="Region" onChange={(e) => this.handleOnChange("address", {...this.state.address, "region": e.target.value})} value={this.state.address && this.state.address.region}/>
+                            <TextField
+                                required
+                                id="input-with-icon-grid" 
+                                label="Region" 
+                                onChange={(e) => this.handleOnChange("address", {...this.state.address, "region": e.target.value})}
+                                onBlur={(e) => this.handleOnChange("address", {...this.state.address, "region": e.target.value.trim()})}
+                                value={this.state.address && this.state.address.region}
+                            />
                         </Grid>
                     </Grid>
 
@@ -88,7 +147,17 @@ export default class RatingInformation extends Component {
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="Postal" onChange={(e) => this.handleOnChange("address", {...this.state.address, "postal": e.target.value})} value={this.state.address && this.state.address.postal}/>
+                            <TextField
+                                required
+                                id="input-with-icon-grid" 
+                                type="number" 
+                                label="Postal" 
+                                helperText={this.props.parentErrors.address !== undefined && this.props.parentErrors.address.postal !== undefined ? "Invalid Postal" : null}
+                                error={this.props.parentErrors.address !== undefined && this.props.parentErrors.address.postal !== undefined} 
+                                onChange={(e) => this.handleOnChange("address", {...this.state.address, "postal": e.target.value})}
+                                onBlur={(e) => this.handleOnChange("address", {...this.state.address, "postal": e.target.value.trim()})}
+                                value={this.state.address && this.state.address.postal}
+                            />
                         </Grid>
                     </Grid>
                 </div>
